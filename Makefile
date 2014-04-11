safeng/DecafCompiler_MIPS_Opt

commit ce86deb718498713e5fe467adf9088c94c547d96

Add new files from p5

Makefile
@@ -17,8 +17,7 @@ PRODUCTS = $(COMPILER)
 default: $(PRODUCTS)
 
 # Set up the list of source and object files
-SRCS = ast.cc ast_decl.cc ast_expr.cc ast_stmt.cc ast_type.cc scope.cc \
-	codegen.cc tac.cc mips.cc errors.cc utility.cc main.cc
+SRCS = ast.cc ast_decl.cc ast_expr.cc ast_stmt.cc ast_type.cc codegen.cc tac.cc mips.cc errors.cc utility.cc main.cc
 
 # OBJS can deal with either .cc or .c files listed in SRCS
 OBJS = lex.yy.o y.tab.o $(patsubst %.cc, %.o, $(filter %.cc,$(SRCS))) $(patsubst %.c, %.o, $(filter %.c, $(SRCS)))
@@ -49,7 +48,7 @@ LEXFLAGS = -d
 YACCFLAGS = -dvty
 
 # Link with standard c library, math library, and lex library
-LIBS = -lc -lm -lfl
+LIBS = -lc -lm -ll
 
 # Rules for various parts of the target
 
@@ -93,4 +92,20 @@ clean:
 	rm -f $(JUNK) y.output $(PRODUCTS)
 
 # DO NOT DELETE
-
+ast.o: ast.cc ast.h location.h ast_type.h list.h utility.h ast_decl.h
+ast_decl.o: ast_decl.cc ast_decl.h ast.h location.h ast_type.h list.h \
+ utility.h ast_stmt.h
+ast_expr.o: ast_expr.cc ast_expr.h ast.h location.h ast_stmt.h list.h \
+ utility.h ast_type.h ast_decl.h
+ast_stmt.o: ast_stmt.cc ast_stmt.h list.h utility.h ast.h location.h \
+ ast_type.h ast_decl.h ast_expr.h
+ast_type.o: ast_type.cc ast_type.h ast.h location.h list.h utility.h \
+ ast_decl.h
+codegen.o: codegen.cc codegen.h list.h utility.h tac.h mips.h
+tac.o: tac.cc tac.h list.h utility.h mips.h
+mips.o: mips.cc mips.h list.h utility.h tac.h
+errors.o: errors.cc errors.h location.h scanner.h ast_type.h ast.h list.h \
+ utility.h ast_expr.h ast_stmt.h ast_decl.h
+utility.o: utility.cc utility.h list.h
+main.o: main.cc utility.h errors.h location.h parser.h scanner.h list.h \
+ ast.h ast_type.h ast_decl.h ast_expr.h ast_stmt.h y.tab.h

list.h
@@ -31,7 +31,6 @@
 #include <deque>
 #include <algorithm>
 #include "utility.h"  // for Assert()
-#include "scope.h"
   
 class Node;
 class CodeGenerator;
@@ -98,13 +97,6 @@ template<class Element> class List {
     void SetParentAll(Node *p)
         { for (int i = 0; i < NumElements(); i++)
              Nth(i)->SetParent(p); }
-    void DeclareAll(Scope *s)
-        { for (int i = 0; i < NumElements(); i++)
-             s->Declare(Nth(i)); }
-
-   void CheckAll()
-        { for (int i = 0; i < NumElements(); i++)
-             Nth(i)->Check(); }
     void EmitAll(CodeGenerator *cg)
         { for (int i = 0; i < NumElements(); i++)
              Nth(i)->Emit(cg); }

mips.h
@@ -86,6 +86,8 @@ class Mips {
 
     void EmitPreamble();
 
+    void SaveCaller(Location *location);
+    void RestoreCaller(Location *location);
 };
 
 

run
@@ -34,7 +34,7 @@ fi
 
 echo "-- spim -file tmp.asm"
 echo " "
-$SPIM -file tmp.asm
+$SPIM -keepstats -file tmp.asm
 
 echo " "
 echo " "

commit f6340fe7674379d8bf64156200210f67a0b272d5

Fix the library name for Flex
  * Makefile: Fix the library name for Flex. This bug is introduced by
    [ce86deb718498713e5fe467adf9088c94c547d96].

Signed-off-by: Hongjiu Zhang <zhanghj@umich.edu>

Makefile
@@ -49,7 +49,7 @@ LEXFLAGS = -d
 YACCFLAGS = -dvty
 
 # Link with standard c library, math library, and lex library
-LIBS = -lc -lm -ll
+LIBS = -lc -lm -lfl
 
 # Rules for various parts of the target
 